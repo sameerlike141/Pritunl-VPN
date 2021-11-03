5bf1306ef1752900bb8ee4aeacc72fa94a2cca70
sameerlike141 <sameerlike141@gmail.com>
2021-11-03 18:01:39 +0500

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install curl gnupg2 wget unzip -y
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+echo "deb http://repo.pritunl.com/stable/apt focal main" | tee /etc/apt/sources.list.d/pritunl.list
+apt-get update -y
+apt-get install pritunl -y
+systemctl start pritunl
+systemctl enable pritunl
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt-get update -y
+apt-get install mongodb-server -y
