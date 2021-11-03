9be3dbbfc1cded2ca8eb68c4c12b1de4ec496455
sameerlike141 <sameerlike141@gmail.com>
2021-11-03 18:09:25 +0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -13,3 +13,7 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 apt-get update -y
 apt-get install mongodb-server -y
+systemctl start mongodb
+systemctl enable mongodb
+pritunl setup-key
+pritunl default-password
