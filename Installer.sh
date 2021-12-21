0d2de63b8ee34052f26080f5e8b0f0011d29eb7a
sameerlike141 <sameerlike141@gmail.com>
2021-12-21 18:14:07 +0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt focal main
+EOF
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
 
 apt-get update -y
 apt-get install curl gnupg2 wget unzip -y
